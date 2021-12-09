94fb66dee1ed89824ad2f0b595802875ada3b375
That is me <trab.die@gmail.com>
2021-12-09 15:25:26 -0500

    student class

--- src/main/java/io/zipcoder/Student.java
+++ src/main/java/io/zipcoder/Student.java
@@ -1,4 +1,18 @@
 package io.zipcoder;
 
+import java.util.ArrayList;
+
 public class Student {
+    private String firstName;
+    private String lastName;
+    private ArrayList<Double> examScores;
+
+    // Constructor
+    public Student (String firstName,String lastName,ArrayList<Double> examScores ) {
+        this.firstName=firstName;
+        this.lastName=lastName;
+        this.examScores=examScores;
+    }
+
+
 }
