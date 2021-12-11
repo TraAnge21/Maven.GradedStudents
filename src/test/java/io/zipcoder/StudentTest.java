4a8a15c8cca94bd2d1e4e691e621b134b0a4c77e
That is me <trab.die@gmail.com>
2021-12-10 23:06:47 -0500

    testing student

--- src/test/java/io/zipcoder/StudentTest.java
+++ src/test/java/io/zipcoder/StudentTest.java
@@ -1,5 +1,59 @@
 package io.zipcoder;
 
+import org.junit.Before;
+import org.junit.Test;
+
 public class StudentTest {
 
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void getNumberOfExamsTaken() {
+    }
+
+    @Test
+    public void getExamScores() {
+    }
+
+    @Test
+    public void addExamScore() {
+    }
+
+    @Test
+    public void setExamScore() {
+    }
+
+    @Test
+    public void getAverageExamScore() {
+    }
+
+    @Test
+    public void displayAverageExamScore() {
+    }
+
+    @Test
+    public void testToString() {
+
+        //Given
+        String firstName = "Tra";
+        String LastName = " DIE";
+        Double [] examScores= {70.00, 120.00, 80.00};
+        double expected = 90.00;
+
+        // When
+
+        double actual =
+
+
+        // Then
+
+
+
+
+
+
+    }
 }
